4e6209b8d5ee3e740d791c804ced983607d59f13
DanielaNahir <danielanahir927@gmail.com>
2024-10-23 10:17:10 -0300

    splash

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,13 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const splash = document.getElementById('splash-screen');
+    if (splash) {
+      setTimeout(() => {
+        splash.style.opacity = '0';
+        setTimeout(() => splash.remove(), 1000); // Tiempo para animación de desvanecimiento
+      }, 5000); // Tiempo para mostrar el splash screen
+    }
+  });
